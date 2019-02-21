f91bb7d519305b079712bcdf5b6d8f735f5669a9
WilsonIsAlive <33021153+WilsonIsAlive@users.noreply.github.com>
2019-02-21 16:46:53 +0100

    Funcionando
    Revisar que no se haga doble click y te lo de por bueno.

--- src/practica1.js
+++ src/practica1.js
@@ -1,4 +1,8 @@
 /**
+Cosas que hacer, mirar que se vean las cartas 2
+Que se dan 2 click en la misma carta
+
+
  * MemoryGame es la clase que representa nuestro juego. Contiene un array con la cartas del juego,
  * el número de cartas encontradas (para saber cuándo hemos terminado el juego) y un texto con el mensaje
  * que indica en qué estado se encuentra el juego
@@ -8,9 +12,9 @@ var MemoryGame = MemoryGame || {};
 /**
  * Constructora de MemoryGame
  */
- // Esto es un tablero
-MemoryGame = function(gs) {
-	 var sprites = [
+// Esto es un tablero
+MemoryGame = function (gs) {
+	var sprites = [
 		"back",
 		"8-ball",
 		"potato",
@@ -23,29 +27,28 @@ MemoryGame = function(gs) {
 	];
 	var cartas = [];
 	var contadorDeParejas = 8;
-	var comparar = false;
-	var dosCartas = false;
-	var cardId2;
+	var carta1 = null;
+	var carta2 = null;
 	// gs -> es el servidor grafico
-	
+
 	// Inicializo todos los valores de la matriz, con el dibujo
-	this.initGame = function(){
+	this.initGame = function () {
 		var contador = 1;
 		// Creo las cartas y las guardo en el array de Cartas
-		for(var i = 0; i < 16; i++){
+		for (var i = 0; i < 16; i++) {
 			cartas.push(new MemoryGameCard(sprites[contador]));
 			// Inicializo el contador, ya que he metido por lo menos
 			// una vez cada tipo de carta y tengo que meter su pareja
-			if(contador == 8){
+			if (contador == 8) {
 				contador = 0;
 			}
 			contador++;
 		}
 		// Aqui mezclo las cartas unas 50 veces
-		for(var i = 0; i < 50; i++){
+		for (var i = 0; i < 50; i++) {
 			// Me guardo 2 posiciones del array de cartas creadas aleatoriamente
-			var primeraCarta = Math.ceil((Math.random()*16));
-			var segundaCarta = Math.ceil((Math.random()*16));
+			var primeraCarta = Math.ceil((Math.random() * 16));
+			var segundaCarta = Math.ceil((Math.random() * 16));
 			// Le resto menos uno ya que se genera un numero entre 1 y 16
 			primeraCarta--;
 			segundaCarta--;
@@ -58,67 +61,67 @@ MemoryGame = function(gs) {
 		//Llamar a la funcion loop()
 		setInterval(this.loop, 16);
 	};
-	draw = function(){
+	draw = function () {
 		// 1. Escribe el mensaje con el estado actual del juego
-		if(contadorDeParejas != 0){
+		if (contadorDeParejas != 0) {
 			gs.drawMessage("Usted Esta Jugando");
 		}
-		else{
+		else {
 			gs.drawMessage("Usted Ganó");
 		}
-		
+
 		// 2. Pide a cada una de las cartas del tablero que se dibujen
-		for(var i = 0; i < cartas.length; i++){
+		for (var i = 0; i < cartas.length; i++) {
 			cartas[i].draw(gs, i);
 		}
+
+
 	}
 
-	this.loop = function(){
+	this.loop = function () {
 		// Llama a la funcion draw
 		draw();
-		
-	}
-	this.onClick = function(cardId){
-		// Volteo la carta que me pasan por el cardId
-		cartas[cardId].flip();
-		/*if(dosCartas){
-			//this.loop();
-			dosCartas = false;
-		}
-		else{
-			cardId2 = cardId;
-			dosCartas = true;
 
-		}*/
-		var i = 0, encontrado = false;
-		// Compruebo si hay otra carta dada la vuelta tambien, para 
-		// poder hacer la compareción
-		while(i < cartas.length && !encontrado){
-			// Miro que su estado sea "Cara" y que no se la misma que la de cardId
-			if(cartas[i].estado == "Cara" && i != cardId){
-				encontrado = true;
-				// Compruebo si son la misma carta
-				if(cartas[cardId].compareTo(cartas[i])){
-					// Cambio su estado a "Encontada"
-					cartas[cardId].found();
-					cartas[i].found();
-					// Le resto a la varible menos uno, para saber luego si 
-					// el juego a terminado
-					contadorDeParejas--;
-				}
-				// No son la misma, cambio su estado a "Espalda"
-				else{
-					setTimeout(
-					function({
-						// Necesito hacer que se vean el suficiente tiempo las 2 cartas
-						cartas[cardId].flip();
-						cartas[i].flip();
-					}),2000);
-					
-				}
-			}
-			i++;
+	}
+	this.onClick = function (cardId) {
+
+		cartas[cardId].flip();
+		if (carta1 == null) {
+			carta1 = cartas[cardId];
 		}
+		else {
+			carta2 = cartas[cardId];
+		}
+		if (carta1 != null && carta2 != null) {
+			console.log("Else");
+			//console.log("LA carta gurada es esta" + carta.nombre);
+			// Compruebo si son la misma carta
+			if (carta1.compareTo(carta2)) {
+				console.log("IF segundo")
+				// Cambio su estado a "Encontada"
+				carta1.found();
+				//cartas[carta].found();
+				carta2.found();
+				// Le resto a la varible menos uno, para saber luego si 
+				// el juego a terminado
+				contadorDeParejas--;	
+				carta1 = null;
+				carta2 = null;
+			}
+			// No son la misma, cambio su estado a "Espalda"
+			else {
+				console.log("ESLE 2");
+				setTimeout(() => {
+					carta1.flip();
+					carta2.flip();
+					carta1 = null;
+					carta2 = null;
+				}, 250);
+			}
+			
+		}
+		// Volteo la carta que me pasan por el cardId
+
 	}
 };
 
@@ -130,31 +133,31 @@ MemoryGame = function(gs) {
  * La carta puede guardar la posición que ocupa dentro del tablero para luego poder dibujarse
  * @param {string} id Nombre del sprite que representa la carta
  */
- // Esto es una carta
-MemoryGameCard = function(id) {
+// Esto es una carta
+MemoryGameCard = function (id) {
 	this.estado = "Espalda";// ["Encontrado", "Espalda", "Cara"];// array del estado de las cartas
 	this.nombre = id;// Esto es el sprite
 
 	// Cambia el estado de la carta
-	this.flip = function(){
+	this.flip = function () {
 
 		// Para que no se cambie el estado de la carta
-		if(this.estado != "Encontrada"){
-			if(this.estado == "Espalda"){
+		if (this.estado != "Encontrada") {
+			if (this.estado == "Espalda") {
 				this.estado = "Cara";
 			}
-			else if(this.estado == "Cara"){
+			else if (this.estado == "Cara") {
 				this.estado = "Espalda";
 			}
 		}
 		//console.log(this.estado);
 	}
 	// Cambia el estado a Encontrado
-	this.found = function(){
+	this.found = function () {
 		this.estado = "Encontrada";
 	}
 	// Mira si las 2 cartas son iguales
-	this.compareTo = function(otherCard){
+	this.compareTo = function (otherCard) {
 		return this.nombre == otherCard.nombre;
 		// Pongo 2
 		/*if(this.nombre == otherCard.nombre){
@@ -163,12 +166,12 @@ MemoryGameCard = function(id) {
 		return cierto;*/
 	}
 	// Pinta la carta en funcion del estado en el que se encuentre
-	this.draw = function(gs, pos){
+	this.draw = function (gs, pos) {
 		//console.log(this.estado);
-		if(this.estado == "Espalda"){
+		if (this.estado == "Espalda") {
 			gs.draw("back", pos);
 		}
-		else{
+		else {
 			gs.draw(this.nombre, pos);
 		}
 	}
